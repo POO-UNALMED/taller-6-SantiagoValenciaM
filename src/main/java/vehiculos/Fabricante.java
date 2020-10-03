c13f175f3e7e9c762282b975c05b78a691fbb47c
Santiago Valencia Mejía <savalencia@unal.edu.co>
2020-10-02 23:27:40 -0500

    finalV.1

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package vehiculos;
 import java.util.ArrayList;
 
@@ -47,63 +46,4 @@ public class Fabricante {
         }
         return mayorp;
 	}
-}   
-
-			
-
-
-=======
-package vehiculos;
-import java.util.ArrayList;
-
-public class Fabricante {
-	
-	private String nombre;
-	private Pais pais;
-	private int numVehiculos=0;
-	private static ArrayList<Fabricante> numfabric = new ArrayList<Fabricante>();
-	public Fabricante(String nombre, Pais pais) {
-		this.nombre=nombre;
-		this.pais=pais;
-		numfabric.add(this);
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	public String getPais() {
-		return pais;
-	}
-	
-	public void setPais(String pais) {
-		this.pais = pais;
-	}
-	
-	public void getNumVehiculos() {
-		return numVehiculos;
-	}
-	public void addVehiculo() {
-		numVehiculos ++;
-		this.pais.addVehiculo();
-	}
-	
-	public static Fabricante fabricaMayorVentas() {
-        Fabricante mayorp = Fabricante.numfabric.get(0);
-        for (Fabricante fabricante: numfabric){
-            if(mayorp.getNumVehiculos() < fabricante.getNumVehiculos()){
-                mayorp = fabricante;
-            }
-        }
-        return mayorp;
-	}
-}   
-
-			
-
-
->>>>>>> 093a50ce61d7e493ac4d3ef2bfefbeae5fe77d5b
+}
